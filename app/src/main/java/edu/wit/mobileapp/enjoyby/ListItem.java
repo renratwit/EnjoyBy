777420964bb9163cd75bbf57211fe99fd7d9d1f9
renratwit <raymond.ren9@gmail.com>
2021-08-04 09:00:19 -0400

    List Items

---
+++ app/src/main/java/edu/wit/mobileapp/enjoyby/ListItem.java
@@ -0,0 +1,10 @@
+package edu.wit.mobileapp.enjoyby;
+
+import java.util.Date;
+
+public class ListItem {
+    // Maybe an image?
+    public String name;
+    public Date purchaseDate;
+    public Date expireDate;
+}
