73b1943588000865eeb2d6aeefaa10971cc4a418
renratwit <raymond.ren9@gmail.com>
2021-08-05 21:41:41 -0400

    Jason was a silly goose and used the wrong method to getFreshFoodItems()

--- app/src/main/java/edu/wit/mobileapp/enjoyby/All_Fragment.java
+++ app/src/main/java/edu/wit/mobileapp/enjoyby/All_Fragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,11 +24,13 @@ public class All_Fragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.v("myApp", "REACHED ALL FRAGMENT");
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_all, container, false);
 
--- app/src/main/java/edu/wit/mobileapp/enjoyby/MainActivity.java
+++ app/src/main/java/edu/wit/mobileapp/enjoyby/MainActivity.java
@@ -10,6 +10,7 @@ import com.google.android.material.tabs.TabLayout;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.View;
 
 import java.util.Calendar;
@@ -27,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.v("myApp", "Main Activity Created");
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
